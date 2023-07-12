2fbe1cd13351ddeb41b5f20d706774fdf5be6f67
duzhi <duzchih@gmail.com>
2023-07-12 13:00:01 +0800

    init proj & home page demo

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <div class="flex justify-center mt-200px h-81.5">
+    <section class="">
+      <p>你好，欢迎</p>
+      <p>很高兴认识你</p>
+      <p>这里是一个写笔记，写故事的地方</p>
+      <p>或许，也会记一些无聊的事</p>
+    </section>
+  </div>
+  <footer class="h-430px w-full"></footer>
+</template>
+
+<style scoped>
+footer {
+  background-image: url("@/assets/icons/waves.svg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+</style>
