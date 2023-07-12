e365b92172051d5ddcd386d1d2eb0825acca289b
duzhi <duzchih@gmail.com>
2023-07-12 15:39:25 +0800

    figured out image lost druing packing & style adjsut

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 </script>
 
 <template>
-  <div class="flex justify-center mt-200px h-81.5">
-    <section class="">
-      <p>你好，欢迎</p>
-      <p>很高兴认识你</p>
-      <p>这里是一个写笔记，写故事的地方</p>
-      <p>或许，也会记一些无聊的事</p>
+  <div class="flex justify-center pt-200px custom-height">
+    <section class="text-lg">
+      <p>你好，</p>
+      <p>很高兴认识你。</p>
+      <p>这里是一个写笔记，写故事的地方。</p>
+      <p>或许，也会记一些无聊的事。</p>
     </section>
   </div>
   <footer class="h-430px w-full"></footer>
@@ -16,8 +16,14 @@
 
 <style scoped>
 footer {
-  background-image: url("@/assets/icons/waves.svg");
+  background-image: url("./assets/icons/waves.svg");
   background-repeat: no-repeat;
   background-size: 100% 100%;
+  position: absolute;
+  bottom: 0;
+}
+.custom-height {
+  height: calc(100% - 430px);
+  padding-bottom: 430px;
 }
 </style>
