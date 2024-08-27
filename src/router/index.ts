ca90fdb75e34076a6e233114a022976098273d3d
Dchih <duzchih@gmail.com>
2024-08-27 20:32:23 +0800

    mirror page

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,38 +6,43 @@ const BoringThings = () => import("@/views/boringThings/index.vue");
 const Reflections = () => import("@/views/reflections/index.vue");
 const Technics = () => import("@/views/technics/index.vue");
 const Economic = () => import("@/views/economic/index.vue");
+const Mirror  = () => import("@/views/mirror/index.vue")
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      component: Home,
+      path: '/',
+      component: Home
     },
     {
-      path: "/content",
+      path: '/content',
       component: Content,
-      redirect: "technics",
+      redirect: 'technics',
       children: [
         {
-          path: "technics",
-          component: Technics,
+          path: 'technics',
+          component: Technics
         },
         {
-          path: "reflections",
-          component: Reflections,
+          path: 'reflections',
+          component: Reflections
         },
         {
-          path: "boring-things",
-          component: BoringThings,
+          path: 'boring-things',
+          component: BoringThings
         },
         {
-          path: "economic",
-          component: Economic,
+          path: 'economic',
+          component: Economic
         },
-      ],
-    },
-  ],
-});
+        {
+          path: 'mirror',
+          component: Mirror
+        }
+      ]
+    }
+  ]
+})
 
 export default router;
---
+++ src/views/mirror/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+navigator.mediaDevices
+  .getUserMedia({video: true})
+  .then(function (stream) {
+    const video = document.querySelector('video')!;
+    video.srcObject = stream;
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+    document.getElementById('capture')!.addEventListener('click', function() {
+      var canvas = document.getElementById('canvas') as HTMLCanvasElement;
+      var context = canvas!.getContext('2d');
+      // 将当前视频帧绘制到 canvas 上，实现拍照功能
+      context!.drawImage(video, 0, 0, canvas.width, canvas.height);
+    });
+  })
+  .catch(function (err) {
+    /* 处理 error */
+    alert(err);
+  });
+</script>
+
+<template>
+  <video></video>
+  <div id="capture">拍照</div>
+  <canvas id="canvas"></canvas>
+</template>
+
+<style lang="css">
+video {
+  width: 500px;
+  height: auto;
+}
+
+canvas {
+  margin-top: 20px;
+  width: 500px;
+}
+</style>
