d04e759bac8ed327335bcb95407ae1badf8805b8
Dchih <duzchih@gmail.com>
2024-08-28 20:35:48 +0800

    auto reload page after deploy3

--- src/views/mirror/index.vue
+++ src/views/mirror/index.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-navigator.mediaDevices
-  .getUserMedia({video: true})
-  .then(function (stream) {
-    var video = document.querySelector('video')!;
-    video.srcObject = stream;
-    video.onloadedmetadata = function(e) {
-      video.play();
-    };
-    document.getElementById('capture')!.addEventListener('click', function() {
-      var canvas = document.getElementById('canvas') as HTMLCanvasElement;
-      var context = canvas!.getContext('2d');
-      context!.drawImage(video, 0, 0, canvas.width, canvas.height);
-    });
-  })
-  .catch(function (err) {
-    alert(err);
-  });
+// navigator.mediaDevices
+//   .getUserMedia({video: true})
+//   .then(function (stream) {
+//     var video = document.querySelector('video')!;
+//     video.srcObject = stream;
+//     video.onloadedmetadata = function(e) {
+//       video.play();
+//     };
+//     document.getElementById('capture')!.addEventListener('click', function() {
+//       var canvas = document.getElementById('canvas') as HTMLCanvasElement;
+//       var context = canvas!.getContext('2d');
+//       context!.drawImage(video, 0, 0, canvas.width, canvas.height);
+//     });
+//   })
+//   .catch(function (err) {
+//     alert(err);
+//   });
 
   onMounted(() => {
     const version = localStorage.getItem("version");
     const currentVersion = __APP_VERSION__;
-    if (version !== currentVersion) {
+    console.log(version, currentVersion.toString());
+    if (version !== currentVersion.toString()) {
       localStorage.setItem("version", currentVersion);
       location.reload();
     }
