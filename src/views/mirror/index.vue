c114e66d53910f985f85f3e7a2d370612efd2e2d
Dchih <duzchih@gmail.com>
2024-08-28 18:14:04 +0800

    test cache

--- src/views/mirror/index.vue
+++ src/views/mirror/index.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-// navigator.mediaDevices
-//   .getUserMedia({video: true})
-//   .then(function (stream) {
-//     var video = document.querySelector('video')!;
-//     video.srcObject = stream;
-//     video.onloadedmetadata = function(e) {
-//       video.play();
-//     };
-//     document.getElementById('capture')!.addEventListener('click', function() {
-//       var canvas = document.getElementById('canvas') as HTMLCanvasElement;
-//       var context = canvas!.getContext('2d');
-//       context!.drawImage(video, 0, 0, canvas.width, canvas.height);
-//     });
-//   })
-//   .catch(function (err) {
-//     alert(err);
-//   });
+navigator.mediaDevices
+  .getUserMedia({video: true})
+  .then(function (stream) {
+    var video = document.querySelector('video')!;
+    video.srcObject = stream;
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+    document.getElementById('capture')!.addEventListener('click', function() {
+      var canvas = document.getElementById('canvas') as HTMLCanvasElement;
+      var context = canvas!.getContext('2d');
+      context!.drawImage(video, 0, 0, canvas.width, canvas.height);
+    });
+  })
+  .catch(function (err) {
+    alert(err);
+  });
 </script>
 
 <template>
   <div class="delete-overflow">
-    <!-- <video></video>
+    <video></video>
     <div id="capture">拍照</div>
-    <canvas id="canvas"></canvas> -->
+    <canvas id="canvas"></canvas>
     <input type="file" accept="image/*">
     <input type="file" >
   </div>
