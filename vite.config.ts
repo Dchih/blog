8c1a1df4e66dafa6e6172df9b2273d7b090da475
chih <duzchih@gmail.com>
2024-09-15 09:58:18 +0800

    change conf

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ fs.writeFileSync('./public/version.json', str_ver)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  // base: "./",
   define: {
     'import.meta.env.VERSION_TIME': JSON.stringify(VERSION_TIME)
   },
